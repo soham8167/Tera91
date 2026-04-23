7eb90fd26c059cecc61b639a0cd3793b164919fe
soham mondal <sohammondal50285@gmail.com>
2026-04-23 13:38:10 +0530

    make responsive

--- script.js
+++ script.js
@@ -1,123 +1,100 @@
-const form = document.getElementById("contactForm");
 
-const name = document.getElementById("name");
-const email = document.getElementById("email");
-const phone = document.getElementById("phone");
-const company = document.getElementById("company");
-const project = document.getElementById("project");
-const message = document.getElementById("message");
+// Select all forms
+const forms = document.querySelectorAll(".contactForm");
 
-const nameError = document.getElementById("nameError");
-const emailError = document.getElementById("emailError");
-const phoneError = document.getElementById("phoneError");
-// const companyError = document.getElementById("companyError");
-// const projectError = document.getElementById("projectError");
-// const messageError = document.getElementById("messageError");
+forms.forEach((form) => {
+  const name = form.querySelector(".name");
+  const email = form.querySelector(".email");
+  const phone = form.querySelector(".phone");
 
-// Show error
-function showError(input, errorEl, message) {
-  errorEl.textContent = message;
-  errorEl.classList.remove("hidden");
-  input.classList.add("border", "border-red-500");
-}
+  const nameError = form.querySelector(".nameError");
+  const emailError = form.querySelector(".emailError");
+  const phoneError = form.querySelector(".phoneError");
 
-// Clear error
-function clearError(input, errorEl) {
-  errorEl.textContent = "";
-  errorEl.classList.add("hidden");
-  input.classList.remove("border", "border-red-500");
-}
+  const scrollBtn = document.getElementById("scrollBtn");
 
-
-
-// Name validation
-name.addEventListener("input", () => {
-  name.value = name.value.replace(/[0-9]/g, "");
-  const regex = /^[A-Za-z\s]*$/;
-
-  if (!regex.test(name.value)) {
-    showError(name, nameError, "Only letters allowed");
-  } else {
-    clearError(name, nameError);
-  }
-});
-
-// Email validation
-email.addEventListener("input", () => {
-  const regex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
-  if (!regex.test(email.value)) {
-    showError(email, emailError, "Enter valid email");
-  } else {
-    clearError(email, emailError);
-  }
-});
-
-// Phone validation
-phone.addEventListener("input", () => {
-  phone.value = phone.value.replace(/\D/g, "");
-
-  if (phone.value.length !== 10) {
-    showError(phone, phoneError, "Enter 10 digit phone number");
-  } else {
-    clearError(phone, phoneError);
-  }
-});
-
-
-
-form.addEventListener("submit", function (e) {
+  scrollBtn.addEventListener("click", function (e) {
   e.preventDefault();
 
-  
-  if (name.value.trim() === "") {
-    showError(name, nameError, "Name is required");
-    name.focus();
-    return;
+  if (window.innerWidth >= 1024) {
+    document
+      .getElementById("contactFormSectionDesktop")
+      .scrollIntoView({ behavior: "smooth" });
+  } else {
+    document
+      .getElementById("contactFormSectionMobile")
+      .scrollIntoView({ behavior: "smooth" });
+  }
+});
+
+  // Show error
+  function showError(input, errorEl, message) {
+    errorEl.textContent = message;
+    errorEl.classList.remove("hidden");
+    input.classList.add("border", "border-red-500");
   }
 
-  // 2. Company
-  // if (company.value.trim() === "") {
-  //   showError(company, companyError, "Company is required");
-  //   company.focus();
-  //   return;
-  // } else {
-  //   clearError(company, companyError);
-  // }
-
-  // 3. Phone
-  if (phone.value.trim() === "") {
-    showError(phone, phoneError, "Phone is required");
-    phone.focus();
-    return;
+  // Clear error
+  function clearError(input, errorEl) {
+    errorEl.textContent = "";
+    errorEl.classList.add("hidden");
+    input.classList.remove("border", "border-red-500");
   }
 
-  // 4. Email
-  if (email.value.trim() === "") {
-    showError(email, emailError, "Email is required");
-    email.focus();
-    return;
-  }
+  // Name validation
+  name.addEventListener("input", () => {
+    name.value = name.value.replace(/[0-9]/g, "");
 
-  // 5. Project
-  // if (project.value.trim() === "") {
-  //   showError(project, projectError, "Project type is required");
-  //   project.focus();
-  //   return;
-  // } else {
-  //   clearError(project, projectError);
-  // }
+    const regex = /^[A-Za-z\s]*$/;
 
-  // 6. Message
-  // if (message.value.trim() === "") {
-  //   showError(message, messageError, "Message is required");
-  //   message.focus();
-  //   return;
-  // } else {
-  //   clearError(message, messageError);
-  // }
+    if (!regex.test(name.value)) {
+      showError(name, nameError, "Only letters allowed");
+    } else {
+      clearError(name, nameError);
+    }
+  });
 
- 
+  // Email validation
+  email.addEventListener("input", () => {
+    const regex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
 
-  
-  alert("Form submitted successfully!");
+    if (!regex.test(email.value)) {
+      showError(email, emailError, "Enter valid email");
+    } else {
+      clearError(email, emailError);
+    }
+  });
+
+  // Phone validation
+  phone.addEventListener("input", () => {
+    phone.value = phone.value.replace(/\D/g, "");
+
+    if (phone.value.length !== 10) {
+      showError(phone, phoneError, "Enter 10 digit phone number");
+    } else {
+      clearError(phone, phoneError);
+    }
+  });
+
+  // Submit validation
+  form.addEventListener("submit", function (e) {
+    e.preventDefault();
+
+    if (name.value.trim() === "") {
+      showError(name, nameError, "Name is required");
+      return;
+    }
+
+    if (phone.value.trim() === "") {
+      showError(phone, phoneError, "Phone is required");
+      return;
+    }
+
+    if (email.value.trim() === "") {
+      showError(email, emailError, "Email is required");
+      return;
+    }
+
+    alert("Form submitted successfully!");
+  });
 });
